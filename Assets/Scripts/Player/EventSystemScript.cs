359baace6dcc9658e713d5363f697fd06ec91ac3
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2024-02-11 02:40:56 +0900

    [add] ファイル移動とシングルトンの適用

---
+++ Assets/Scripts/Player/EventSystemScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class EventSystemScript : MonoBehaviour, IPointerClickHandler, ICanvasRaycastFilter
+{
+    [SerializeField] private UnityEvent _event;
+    private const float Radius = 220;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("OnClick!");
+        _event.Invoke();
+    }
+
+    public bool IsRaycastLocationValid(Vector2 sp, Camera eventCamera)
+    {
+        return Vector2.Distance(sp, transform.position) < Radius;
+    }
+}
---
+++ Assets/Scripts/Player/ResourcesTextController.cs
@@ -0,0 +1,26 @@
+using TMPro;
+using UnityEngine;
+
+public class ResourcesTextController : MonoBehaviour
+{
+    private TextMeshProUGUI _text;
+    private PlayerManager _playerManager = PlayerManager.Instance;
+
+    private void Start()
+    {
+        _text = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        UpdateText();
+    }
+
+    /// <summary>
+    /// UnityEvent登録用テキスト更新メソッド
+    /// </summary>
+    public void UpdateText()
+    {
+        _text.text = _playerManager.PlayerResources.ToString("F2");
+    }
+}
