232c73f2a8c8098b2c12186a0910548e9ddcff5d
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2024-02-15 15:47:34 +0900

    [add] ゴールデンクッキー取得状態を管理するbool追加、ゴールデンクッキー状態の生産量増加処理追加

--- Assets/Scripts/Player/CookieGenerator.cs
+++ Assets/Scripts/Player/CookieGenerator.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class CookieGenerator : MonoBehaviour
 {
+    private const int GoldenCookieAddValue = 7;
     private readonly PlayerManager _playerManager = PlayerManager.Instance;
 
     private void Start()
@@ -12,20 +13,31 @@ public class CookieGenerator : MonoBehaviour
         var ct = this.GetCancellationTokenOnDestroy();
         AutoGenerate(ct).Forget();
     }
-    
+
     // TODO 一応自動用と手動用で分けているが、統一出来そうな気もする
 
     /// <summary>
     /// リソース自動生産メソッド
     /// </summary>
-    public async UniTaskVoid AutoGenerate(CancellationToken ct)
+    private async UniTaskVoid AutoGenerate(CancellationToken ct)
     {
         while (true)
         {
-            foreach (var item in _playerManager.AutoGeneratorDictionary.Values)
+            if (_playerManager.IsGoldenCookieMode)
             {
-                _playerManager.AddCookie(item.BaseGeneratorValue * (1 << (int)item.BasePower));
+                foreach (var item in _playerManager.AutoGeneratorDictionary.Values)
+                {
+                    _playerManager.AddCookie((item.BaseGeneratorValue * (1 << (int)item.BasePower)) * GoldenCookieAddValue);
+                }
             }
+            else
+            {
+                foreach (var item in _playerManager.AutoGeneratorDictionary.Values)
+                {
+                    _playerManager.AddCookie(item.BaseGeneratorValue * (1 << (int)item.BasePower));
+                }
+            }
+
             await UniTask.Delay(TimeSpan.FromSeconds(1), cancellationToken: ct);
         }
     }
