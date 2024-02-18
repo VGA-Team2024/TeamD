f1966b8f8761977dbf8c5f8fcb3742b975733eaf
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2024-02-18 17:34:00 +0900

    [update] 日本語表記と英語表記を切り替え可能に変更

--- Assets/Scripts/Player/ResourcesTextController.cs
+++ Assets/Scripts/Player/ResourcesTextController.cs
@@ -9,9 +9,15 @@ public class ResourcesTextController : MonoBehaviour
     private PlayerManager _playerManager = PlayerManager.Instance;
 
     // 英語表現での最大桁数
-    private const int MaxDigit = 99;
-    
-    public static readonly string[] _scales =
+    private const int MaxDigitEn = 99;
+    private const int MaxDigitJp = 68;
+
+    public static readonly string[] ScalesJp =
+    {
+        "", "万", "億", "兆", "京", "垓", "秭", "穣", "溝", "澗", "正", "載", "極", "恒河沙", "阿僧祇", "那由多", "不可思議", "無量大数"
+    };
+
+    public static readonly string[] ScalesEn =
     {
         // 参考サイト : https://nanaon.tamura-shippo.com/entertainment/game/cookie-clicker/unit/
         "", "", "", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion", "septillion",
@@ -22,14 +28,24 @@ public class ResourcesTextController : MonoBehaviour
         "untrigintillion", "duotrigintillion"
     };
 
+    private enum LanguageType
+    {
+        En,
+        Jp
+    }
+
+    [SerializeField] private LanguageType _type;
+    private static LanguageType _languageMode;
+
     private void Start()
     {
         _text = GetComponent<TextMeshProUGUI>();
+        _languageMode = _type;
     }
 
     private void Update()
     {
-        var str = UseDigitRepresentation(_playerManager.CookieCount * 1000000).Split();
+        var str = UseDigitRepresentation(_playerManager.CookieCount).Split();
         _text.text = $"{str[0]}\n{str[1]}クッキー";
     }
 
@@ -39,21 +55,44 @@ public class ResourcesTextController : MonoBehaviour
     public static string UseDigitRepresentation(double value)
     {
         var count = Math.Log10(Math.Truncate(value));
-        if (MaxDigit <= count)
+        switch (_languageMode)
         {
-            return $"{value / Math.Pow(10, MaxDigit):F2} {_scales[_scales.Length]}";
+            case LanguageType.En:
+                if (MaxDigitEn <= count)
+                {
+                    return $"{value / Math.Pow(10, MaxDigitEn):F2} {ScalesEn[ScalesEn.Length]}";
+                }
+
+                if (count < 6)
+                {
+                    return $"{(value):F2} ";
+                }
+
+                if (count % 3 == 0)
+                {
+                    return $"{value / Math.Pow(10, count):F2} {ScalesEn[(int)count / 3]}";
+                }
+
+                return $"{value / Math.Pow(10, count - count % 3):F2} {ScalesEn[(int)count / 3]}";
+            case LanguageType.Jp:
+                if (MaxDigitJp <= count)
+                {
+                    return $"{value / Math.Pow(10, MaxDigitJp):F2} {ScalesJp[ScalesJp.Length]}";
+                }
+
+                if (count < 4)
+                {
+                    return $"{(value):F2} ";
+                }
+
+                if (count % 4 == 0)
+                {
+                    return $"{value / Math.Pow(10, count):F2} {ScalesJp[(int)count / 4]}";
+                }
+
+                return $"{value / Math.Pow(10, count - count % 4):F2} {ScalesJp[(int)count / 4]}";
         }
 
-        if (count < 6)
-        {
-            return $"{(value):F2} ";
-        }
-
-        if (count % 3 == 0)
-        {
-            return $"{value / Math.Pow(10, count):F2} {_scales[(int)count / 3]}";
-        }
-
-        return $"{value / Math.Pow(10, count - count % 3):F2} {_scales[(int)count / 3]}";
+        return "Enum未設定";
     }
 }
