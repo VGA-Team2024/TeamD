8b5b02fef485259d1687077dd12e4b13891b4751
Mikuto <134473854+Mikuto-Takechi@users.noreply.github.com>
2024-02-20 13:33:24 +0900

    [update]自動生産で1秒毎にnクッキーを瞬間的に増やしていた処理を、毎フレーム(n*前フレームからの経過時間[s])クッキー生産する処理に変更した。 [update]自動生産する際に毎回その場で加算量を計算していた処理を中止し、StatsManagerで予め加算量を計算しておき、その数値を利用してクッキーを加算する処理に移行した。

--- Assets/WorkSpace/Takechi/StatsManager.cs
+++ Assets/WorkSpace/Takechi/StatsManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using TeamD.Enum;
@@ -17,10 +18,46 @@ public static class StatsManager
     /// <summary>施設のアップグレード状況と数</summary>
     public static ReactiveDictionary<FactoryKey, (UpgradeTier Tier, int Amount)> FactoryStats { get; set; }
 
+    /// <summary>購入可能なアップグレードのディクショナリ</summary>
+    static Dictionary<FactoriesEntity, (TiersEntity Tier, UpgradesEntity UpgradesInfo)> _nextUpgrades = new();
+    public static Subject<Dictionary<FactoriesEntity, (TiersEntity Tier, UpgradesEntity UpgradesInfo)>> OnUpdateNextUpgrades { get; } = new();
+    /// <summary>Cookie Per Seconds: 一秒間に生成するクッキー数</summary>
+    public static double CpS { get; private set; }
+
+    static Factories _factories;
+    static Tiers _tiers;
+    static Upgrades _upgrades;
+
+    static void UpdateCpS()
+    {
+        CpS = FactoryStats.Select(x => _factories.Entities
+            .Find(e => e.Key == x.Key).CpS * x.Value.Amount * (1 << (int)x.Value.Tier)).Sum();
+    }
+
+    static void UpdateNextUpgrades()
+    {
+        _nextUpgrades.Clear();
+        foreach (var pair in FactoryStats)
+        {
+            var nextTier = _tiers.Entities.FirstOrDefault(x => x.Tier == pair.Value.Tier + 1);
+            if (nextTier == null) continue;
+            if (nextTier.Condition <= pair.Value.Amount)
+            {
+                _nextUpgrades.Add(_factories.Entities
+                    .First(e=>e.Key.Equals(pair.Key)), (nextTier, _upgrades.Entities
+                    .FirstOrDefault(e=>e.Key.Equals(pair.Key) && e.Tier.Equals(nextTier.Tier))));
+            }
+        }
+        OnUpdateNextUpgrades.OnNext(_nextUpgrades);
+    }
     static StatsManager()
     {
-        var factories = Resources.Load<Factories>("Excel/Factories");
-        FactoryStats = factories.Entities.ToDictionary(entity=>entity.Key, _=>(UpgradeTier.NoUpgrade, 0))
+        _factories = Resources.Load<Factories>("Excel/Factories");
+        _tiers = Resources.Load<Tiers>("Excel/Tiers");
+        _upgrades = Resources.Load<Upgrades>("Excel/Upgrades");
+        FactoryStats = _factories.Entities.ToDictionary(entity=>entity.Key, _=>(UpgradeTier.NoUpgrade, 0))
             .ToReactiveDictionary();
+        FactoryStats.ObserveReplace().Subscribe(_ => UpdateCpS());
+        FactoryStats.ObserveReplace().Subscribe(_ => UpdateNextUpgrades());
     }
 }
