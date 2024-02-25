75778786abae4ddad6d3752031a1cb5929913296
Mikuto <134473854+Mikuto-Takechi@users.noreply.github.com>
2024-02-25 17:58:32 +0900

    [update]EventSystemScriptにDOTweenでアニメーションを追加した。

--- Assets/Scripts/Player/EventSystemScript.cs
+++ Assets/Scripts/Player/EventSystemScript.cs
@@ -1,19 +1,73 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
-public class EventSystemScript : MonoBehaviour, IPointerClickHandler, ICanvasRaycastFilter
+public class EventSystemScript : 
+    MonoBehaviour, ICanvasRaycastFilter, 
+    IPointerEnterHandler, IPointerExitHandler, 
+    IPointerDownHandler, IPointerUpHandler
 {
     [SerializeField] private UnityEvent _event;
-    private const float Radius = 220;
+    float _baseRadius, _radius;
+    bool _pointerEnter;
+    Tween _tween;
+    float _prevSize = 1;
 
-    public void OnPointerClick(PointerEventData eventData)
+    void Awake()
     {
-        _event.Invoke();
+        _baseRadius = GetComponent<RectTransform>().rect.width / 2;
+        _radius = _baseRadius;
     }
-
+    /// <summary>サイズ変更アニメーション</summary>
+    void Resize(float size)
+    {
+        _tween?.Kill();
+        //  DoScaleだとTween途中のサイズ->変更後サイズをベースにイージングされるので
+        //  DoVirtual.Floatで変更前サイズ->変更後サイズをベースにイージングを作ることで
+        //  大きさを一定に保つことができた
+        _tween = DOVirtual.Float(_prevSize, size, 1f, f =>
+        {
+            if (_prevSize >= size && transform.localScale[0] >= f)
+            {
+                transform.localScale = new Vector3(f,f,f);
+            }
+            if (_prevSize <= size && transform.localScale[0] <= f)
+            {
+                transform.localScale = new Vector3(f,f,f);
+            }
+        }).SetEase(Ease.OutElastic).SetLink(gameObject);
+        _prevSize = size;
+        //  マウスカーソルの接触判定を取るための半径をscaleに合わせる
+        _radius = _baseRadius * size;
+    }
+    /// <summary>
+    /// マウスカーソルの接触判定
+    /// </summary>
     public bool IsRaycastLocationValid(Vector2 sp, Camera eventCamera)
     {
-        return Vector2.Distance(sp, transform.position) < Radius;
+        return ((Vector3)sp - transform.position).sqrMagnitude < _radius * _radius;
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _pointerEnter = true;
+        Resize(1.05f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _pointerEnter = false;
+        Resize(1f);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _event.Invoke();
+        Resize(0.95f);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if(_pointerEnter) Resize(1.05f);
     }
 }
