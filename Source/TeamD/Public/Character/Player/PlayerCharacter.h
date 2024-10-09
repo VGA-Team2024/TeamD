4f5d47f1379f7888a7ca4e80fa56615afbad353b
showgo-koinuma <van71413989i@gmail.com>
2024-10-09 20:04:17 +0900

    [add] PlayerBlueprintの初期設定

---
+++ Source/TeamD/Public/Character/Player/PlayerCharacter.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/CharacterBase.h"
+#include "InputMappingContext.h"
+#include "InputAction.h"
+#include "InputActionValue.h"
+#include "PlayerCharacter.generated.h"
+
+UCLASS()
+class TEAMD_API APlayerCharacter : public ACharacterBase
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+	
+//------------------------input------------------------
+	
+	// MappingContext
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input)
+	TObjectPtr<UInputMappingContext> DefaultMappingContext;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input)
+	TObjectPtr<UInputAction> MoveInput;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input)
+	TObjectPtr<UInputAction> LookInput;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input)
+	TObjectPtr<UInputAction> DodgeInput;
+
+private:
+	// input設定
+	void SetupInput();
+
+	// イベントハンドラー関数
+	// Move
+	void MovePlayer(const FInputActionValue& Value);
+	// Look
+	void RotateControllerInput(const FInputActionValue& Value);
+	// Dodge
+	void PressedDodge();
+	void ReleasedDodge();
+};
