71c5012153d9ccdc4a55564b224f3695664ab4bb
showgo-koinuma <van71413989i@gmail.com>
2024-12-07 20:25:20 +0900

    [add] ai進めた

--- Source/TeamD/Public/Character/Monster/MonsterAIController.h
+++ Source/TeamD/Public/Character/Monster/MonsterAIController.h
@@ -19,5 +19,24 @@ public:
 
 	// 使用するBehaviorTree
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = BehaviorTree)
-	TObjectPtr<UBehaviorTree> BehaviorTree;
+	TObjectPtr<UBehaviorTree> BehaviorTree;// 感知更新時に呼ばれる関数
+	
+protected:
+	// Perceptionの更新を受け取る
+	UFUNCTION()
+	void OnPerceptionUpdated(const TArray<AActor*>& UpdatedActors);
+
+	// SetValueKeyNames
+
+	// 自分
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = KeyName)
+	FName SelfActorKeyName;
+
+	// ターゲットActor
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = KeyName)
+	FName TargetActorKeyName;
+
+	// AIPerception
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = AIPerception)
+	TObjectPtr<UAIPerceptionComponent> AIPerceptionComponent;
 };
